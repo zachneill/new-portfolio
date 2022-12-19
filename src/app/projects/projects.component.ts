8c4b32ca8bc4176eb30913ab9d7774485066d823
zachneill <zacharyneill@gmail.com>
2022-12-19 13:12:10 -0500

    swaps between images

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,8 +7,18 @@ import {Global} from "../global";
   styleUrls: ['./projects.component.sass']
 })
 export class ProjectsComponent {
+  image: string = 'bcvm';
 
   constructor( public global: Global) {
   }
 
+  handleNext() {
+    if (this.image === 'bcvm'){
+      this.global.changeImage('sigbuild')
+      this.image = 'sigbuild'
+    } else if (this.image === 'sigbuild'){
+      this.global.changeImage('bcvm')
+      this.image = 'bcvm'
+    }
+  }
 }
